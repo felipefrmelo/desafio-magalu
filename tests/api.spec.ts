a45c2af244961f55a219272e5ae32b956becf1fe
felipe <felipefrmelo@gmail.com>
2021-11-25 06:01:37 -0300

    Add new end2end test to get a scheduled

--- tests/api.spec.ts
+++ tests/api.spec.ts
@@ -13,4 +13,25 @@ describe("API", () => {
     });
     expect(response.status).toBe(201);
   });
+
+  it("should get a scheduled message", async () => {
+    const id = v4();
+    await request(app).post("/schedule").send({
+      id,
+      to: "42",
+      message: "Hello World",
+      scheduled_at: "2021-11-24T00:00:00.000Z",
+      channel: "sms",
+    });
+    const response = await request(app).get(`/schedule/${id}`);
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({
+      id,
+      to: "42",
+      message: "Hello World",
+      scheduled_at: "2021-11-24T00:00:00.000Z",
+      channel: "sms",
+      status: "pending",
+    });
+  });
 });
