307121d1ab496263acbba6acc9d2bca32835def7
felipe <felipefrmelo@gmail.com>
2021-11-25 11:35:00 -0300

    make second usecase pass :D

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,31 @@
 import express from "express";
+import { newClient, PostgresRepository } from "./adapters/postgresRepository";
+import { ScheduleService } from "./service_layer/scheduleService";
 
 const app = express();
-
 app.use(express.json());
 
-app.post("/schedule", (req, res) => {
-  return res.status(201).send();
-});
+async function init() {
+  const client = await newClient();
+
+  const postgresRepository = new PostgresRepository(client);
+  const service = new ScheduleService(postgresRepository);
+
+  app.post("/schedule", async (req, res) => {
+    const { body } = req;
+    await service.createSchedule(body);
+    return res.status(201).send();
+  });
+
+  app.get("/schedule/:id", async (req, res) => {
+    const { id } = req.params;
+    const schedule = await service.getSchedule(id);
+    return res.status(200).json(schedule);
+  });
+
+  return app;
+}
+
+app.init = init;
 
 export default app;
--- tests/e2e/api.spec.ts
+++ tests/e2e/api.spec.ts
@@ -5,14 +5,17 @@ import { Channel, Status } from "../../src/domain/enums";
 import { makeCreateScheduleCommand } from "../utils";
 
 describe("API", () => {
+  beforeAll(async () => {
+    await app.init();
+  });
+
   it("should return 201 when schedule a message", async () => {
     const cmd = makeCreateScheduleCommand();
     const response = await request(app).post("/schedule").send(cmd);
     expect(response.status).toBe(201);
   });
 
-  it.skip("should get a scheduled message", async () => {
-    const id = v4();
+  it("should get a scheduled message", async () => {
     const cmd = makeCreateScheduleCommand();
     await request(app).post("/schedule").send(cmd);
     const response = await request(app).get(`/schedule/${cmd.id}`);
